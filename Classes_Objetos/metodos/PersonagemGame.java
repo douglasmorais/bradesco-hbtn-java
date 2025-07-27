01d3a8a7201639cdc0fffd0684d773fe48332c55
Douglas Morais <douglas.morais.dasilva@gmail.com>
2025-07-27 18:25:49 -0300

    2.Métodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,36 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if(saudeAtual - quantidadeDeDano) >= 0 {
+            setSaudeAtual(saudeAtual - quantidadeDeDano);
+        } else {
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if(saudeAtual + quantidadeDeCura) <= 100 {
+            setSaudeAtual(saudeAtual + quantidadeDeCura);
+        } else {
+            setSaudeAtual(100);
+        }
+    }
+}
