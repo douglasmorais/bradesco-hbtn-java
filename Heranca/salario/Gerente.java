455608df7beaedc185334ef05d959d833b0101c5
Douglas Morais <douglas.morais.dasilva@gmail.com>
2025-07-29 02:09:54 +0000

    1.Salario

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -2,7 +2,7 @@ public class Gerente extends Empregado {
     public double calcularBonus(Departamento departamento) {
         double bonus = 0;
         if(departamento.alcancouMeta() == true) {
-            bonus = (0.2 * this.salarioFixo) + (0.01 * (departamento.valorAtingioMeta - departamento.valorMeta));
+            bonus = (0.2 * this.salarioFixo) + (0.01 * (departamento.getValorAtingidoMeta() - departamento.getValorMeta()));
         }
         return bonus;
     }
