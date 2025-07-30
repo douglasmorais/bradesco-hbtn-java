664581b6b117f4f4935636e8641aa5185417fa25
Douglas Morais <douglas.morais.dasilva@gmail.com>
2025-07-29 23:22:25 -0300

    5.Quadrado

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,21 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado < 0) {
+            throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            this.lado = lado;
+            setLargura(lado);
+            setAltura(lado);
+        }
+    }
+
+    public quadrado() {
+        super();
+    }
+}
