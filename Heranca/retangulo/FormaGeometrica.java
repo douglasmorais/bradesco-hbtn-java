4bd55fa84e262c48c6e5d02ed5136e7e9fac8428
Douglas Morais <douglas.morais.dasilva@gmail.com>
2025-07-29 22:28:33 -0300

    3.Retângulo

--- Heranca/retangulo/FormaGeometrica.java
+++ Heranca/retangulo/FormaGeometrica.java
@@ -1,3 +1,5 @@
 public class FormaGeometrica {
-    throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    public double area() {
+        throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    }
 }
