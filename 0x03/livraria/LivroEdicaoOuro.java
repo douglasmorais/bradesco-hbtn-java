d97062e32feb86d3dd71bb247def7d51e37cd46b
Douglas Morais <douglas.morais.dasilva@gmail.com>
2025-10-15 17:11:33 -0300

    0.Livraria

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,7 +1,5 @@
-package livraria;
-
-import livraria.exceptions.AutorInvalidoException;
-import livraria.exceptions.LivroInvalidoException;
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
 
 public class LivroEdicaoOuro extends Livro {
     public LivroEdicaoOuro(String titulo, String autor, double preco) throws AutorInvalidoException, LivroInvalidoException {
